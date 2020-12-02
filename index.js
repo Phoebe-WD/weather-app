7ab7ae5ed33dc67c33c59af3d1f53896bb219e81
Phoebe W <phoebe.wilckens@gmail.com>
2020-12-02 10:27:52 -0300

    Creation of the js file and integrated project

---
+++ index.js
@@ -0,0 +1,70 @@
+//Format Date start//
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thusday",
+    "Friday",
+    "Saturday"
+  ];
+  let currentlyDay = date.getDay();
+  let day = days[currentlyDay];
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hour}:${minutes}`;
+}
+let currentlyDate = document.querySelector("#currently-date");
+let currentTime = new Date();
+currentlyDate.innerHTML = formatDate(currentTime);
+//Format Date End//
+
+function showTemp(response) {
+  document.querySelector("#currently-city").innerHTML = response.data.name;
+  document.querySelector("#temp-change").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#weather").innerHTML = response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "89c34fbb589f31537eeb4412cedf92ce";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#validationTooltipUsername").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiKey = "89c34fbb589f31537eeb4412cedf92ce";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemp);
+}
+function showCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+let location = document.querySelector("#search-location");
+location.addEventListener("click", showCurrentLocation);
+
+searchCity("New York");
